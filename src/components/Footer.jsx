312d5056d13f4b383706b7d1102b0ad151d42fbd
Adem-Tozlu <Adem-Tozlu@hotmail.de>
2024-05-24 10:45:10 +0200

    Footer style hinzugefügt und bearbeitet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,21 @@ import { BsTelephone } from "react-icons/bs";
 import { MdOutlineMail } from "react-icons/md";
 import { CiLocationOn } from "react-icons/ci";
 import { RiMailSendLine } from "react-icons/ri";
+import logo from "../images/logo.png"
+import "../components/Footer.css"
+
 
 function Footer() {
     return (
         <>  <footer className='footer'>
-            <div className='footer-container_1'><h3>Hayadmu</h3>
+            <img className='logo-bild' src={logo}alt="logo-bild" />
+            <div className='footer-container_1'>
                 <p>Relax in a Serene and Tranquil Paradise</p>
-                <p>< RiFacebookFill /><IoLogoInstagram /></p> </div>
+                <button className='button-icon'>< RiFacebookFill /></button><button className='button-icon button-icon2'><IoLogoInstagram /></button> </div>
                 <div className='footer-container_2'>
                     <h4 className='contacttext'>CONTACT US</h4>
-                    <p><BsTelephone /> 0945 635 0078</p>
-                    <p><MdOutlineMail /> Hayadmu@gmail.com</p>
+                    <p><BsTelephone /> (+49 30) 321 84 21</p>
+                    <p><MdOutlineMail />  hayadmu@gmail.com</p>
                     <p><CiLocationOn /> Lindenerstr 5,12345 Berlin <p className='land'>Deutschland</p>  </p>
 
                    
@@ -23,8 +27,8 @@ function Footer() {
                 <div className='footer-container_3'>
                 <h4 className='newlatesttext'>NEW LATEST</h4>
                 <p>Get the latest updates and offers.</p>
-                <form >
-                 <input type="email" name="email" id="email" placeholder='Email...' /><button><RiMailSendLine /></button>
+                <form  className='form-email'>
+                 <input type="email" name="email" id="email" placeholder='Email...' /><button className='email'><RiMailSendLine /></button>
                 </form>
 
                 </div>
